e2f9e8762271c3e2600c7205a42d1fe413448b26
eskalona17 <eskalona_17@hotmail.com>
2020-05-17 21:24:09 +0200

    styling components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import './App.css';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media(min-width){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`
 
 function App() {
   return (
-    <h1>Hola mundo</h1>
+    <Contenedor>
+      <div><Imagen srce={imagen} /></div>
+      <div></div>
+    </Contenedor>
   );
 }
 
