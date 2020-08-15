9a029197d42db34d31c1ec776e15ede6f613e4d4
eskalona17 <eskalona_17@hotmail.com>
2020-08-15 17:00:56 +0200

    Updates

---
+++ precache-manifest.0dabf5977d44c6a00031d2af4a5154b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a57cfccecadf0d8f435133ec8dc3e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "101179467fa68d899cf2",
+    "url": "./static/css/main.08b449d4.chunk.css"
+  },
+  {
+    "revision": "06ce19ea225d70353fe0",
+    "url": "./static/js/2.e28a3096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e28a3096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101179467fa68d899cf2",
+    "url": "./static/js/main.dfa863a3.chunk.js"
+  },
+  {
+    "revision": "8c58368de08b69b2f5cf",
+    "url": "./static/js/runtime-main.37a202c8.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "./static/media/cryptomonedas.96e45d69.png"
+  }
+]);
